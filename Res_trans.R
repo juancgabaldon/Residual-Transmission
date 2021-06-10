cfe1600515e1bac8b00a3b429b43ce54b232b8ef
Juan C Gabaldon <77018852+juancgabaldon@users.noreply.github.com>
2021-06-10 17:54:27 +0200

    Update Res_trans.R
    Slightly modified code to include mosquitos from other regions apart from Africa, based on complementary information gathered from similar studies.

--- Res_trans.R
+++ Res_trans.R
@@ -5,6 +5,7 @@ library(shiny)
 library(ggplot2)
 #install.packages("plotly")
 library(plotly)
+library(writexl)
 
 #Reading files
 
@@ -71,19 +72,37 @@ df <-
   bind_rows(Bion_Am %>% mutate(region = 'Americas')) %>%
   bind_rows(Bion_AsPac %>% mutate(region = 'Asian Pacific'))
 
-# Make a quick and dirty plot
-pd <- df %>%
-  dplyr::select(hbi = indoor_host,
-                indoor_biting, region,species,
-                contains('combined_')) %>%
-  tidyr::gather(key, value, combined_1830_2130:combined_0330_0630) %>%
-  mutate(key = gsub('combined_', '', key))
+#Calculating the overall HBI (including outdoors, indoors, combine and others)
+
+df1 <-df%>%rowwise()%>% mutate(total_host =sum(indoor_host_n,outdoor_host_n, combined_host_n,
+                       other_host_n,na.rm=TRUE)/
+                    sum(indoor_host_total, outdoor_host_total, 
+                        combined_host_total,
+                        other_host_total, na.rm=TRUE)*100)
+
+# Make a quick and dirty plot using "matches" instead of "contains"
+
+#We now use the df2 dataframe, constructed based on the "bionomics" file
+
+#df2 is a version of the original file with less variables
+
+#it also has complementary information to fill blank spaces of interest
+
+pd <- df2%>%
+  dplyr::select(hbi = total_host,
+                indoor_biting, region,species, host_unit,
+                matches('combined_|indoor_|outdoor_')) %>%
+  tidyr::gather(key, value, combined_1830_2130:combined_0330_0630|indoor_1830_2130:indoor_0330_0630|outdoor_1830_2130:outdoor_0330_0630) %>%
+  mutate(key = gsub('combined_|indoor_|outdoor_', '', key))%>%
+  filter(value!="")
+  
+
+#write_xlsx(df1,"C:\\Users\\juanc\\Desktop\\Seminarios\\Clases\\Rstudio\\test1\\Residual_transmission\\bionomics.xlsx")
+
 
 #Trying Plotly
 
 fig3D <- plot_ly(pd, x = ~key, y = ~hbi, z = ~indoor_biting)%>%
- add_markers(color= ~region)
-# fig3D
-
-
+  add_markers(color= ~species)
+fig3D
 
